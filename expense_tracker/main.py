ac475bd23b4b23bd91e800e819cd71b7385870f0
umesh induranga <umeshinduranga123@gmail.com>
2025-07-23 11:27:35 +0530

    Update main.py

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -3,26 +3,28 @@ from expense_manager import *
 def show_menu():
     print("\n== Personal Expense Tracker ==")
     print("1. Add Expense")
-    print("2. View Expenses")
-    print("3. Delete Expense")
-    print("4. Monthly Report")
-    print("5. Exit")
+    print("2. View All Expenses")
+    print("3. View Expenses by Category")
+    print("4. Delete Expense")
+    print("5. Monthly Report")
+    print("6. Exit")
 
 while True:
     show_menu()
-    choice = input("Enter your choice (1-5): ")
+    choice = input("Enter your choice (1-6): ")
     
     if choice == '1':
         add_expense()
     elif choice == '2':
         view_expenses()
     elif choice == '3':
-        delete_expense()
+        view_by_category()
     elif choice == '4':
-        monthly_report()
+        delete_expense()
     elif choice == '5':
+        monthly_report()
+    elif choice == '6':
         print("Exiting...")
         break
     else:
-        print("Invalid input, try again.")
-
+        print("Invalid input, please choose between 1-6.")
